bf88be797f04dbed866eab1e239bd2f2d9f4d4fa
Doridian <git@doridian.net>
2022-03-20 14:16:37 -0700

    stop them first to make sure we can activate the raids

--- mdraid-enable.sh
+++ mdraid-enable.sh
@@ -2,10 +2,13 @@
 
 mdadm=/usr/sbin/mdadm
 
+$mdadm --stop md1
+$mdadm --stop md2
+
 $mdadm --assemble --scan
 
-#$mdadm --run --readwrite md0
 $mdadm --run --readwrite md1
 $mdadm --run --readwrite md2
 
 exit 0
+
