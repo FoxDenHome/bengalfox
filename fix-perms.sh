e7526633492abb2d5e91629ff79efe5b7fe17910
Mark Dietzer <git@doridian.net>
2022-05-20 18:58:44 -0700

    Add

---
+++ fix-perms.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+fixperms() {
+	find ! -perm 0664 -type f -print -exec chmod 664 {} \;
+	find ! -perm 2775 -type d -print -exec chmod 2775 {} \;
+	find ! -group share -print -exec chgrp share {} \;
+}
+
+cd /mnt/zhdd/nas
+fixperms
+cd /mnt/zssd/nas
+fixperms
