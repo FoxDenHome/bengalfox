eafe811dbd72df1881840e1f49d6f25b8c95ea4d
Doridian <git@doridian.net>
2022-07-10 20:24:46 -0700

    dont compress

--- gocryptfs.sh
+++ gocryptfs.sh
@@ -3,6 +3,6 @@
 umount /mnt/gocryptfs/nas
 gocryptfs --reverse --passfile /mnt/keydisk/gocryptfs-icefox /mnt/zhdd/nas /mnt/gocryptfs/nas
 
-rsync -azvogXAE /mnt/gocryptfs/nas/ nas@icefox.doridian.net:/mnt/zhdd/nas/share/
+rsync -avogXAE /mnt/gocryptfs/nas/ nas@icefox.doridian.net:/mnt/zhdd/nas/share/
 
 umount /mnt/gocryptfs/nas
