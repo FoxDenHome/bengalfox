5a417639ab8b92b164069811c59ef7dc7877b74e
Doridian <git@doridian.net>
2022-07-11 13:07:36 -0700

    rest server

--- backup.sh
+++ backup.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-runbackup() {
+runx() {
     export REPO="$1"
     /opt/backup/run.sh /mnt/zssd/docker /var/lib/docker/volumes /boot/efi /mnt/zhdd/nashome /
 }
 
-runbackup 'b2:bengalfox-backups:/'
-
+runx 'b2:bengalfox-backups:/'
 REST_SERVER_PASSWORD="$(cat /mnt/keydisk/rest-server-password | tr -d '\r\n\t ')"
-runbackup "rest:https://nas:$REST_SERVER_PASSWORD@icefox.doridian.net:nas/main"
+runx "rest:https://nas:$REST_SERVER_PASSWORD@icefox.doridian.net:nas/main"
--- prune.sh
+++ prune.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-runprune() {
+runx() {
     export REPO="$1"
     /opt/backup/prune.sh
 }
 
-runprune 'b2:bengalfox-backups:/'
-runprune 'sftp:nas@icefox.doridian.net:/mnt/zhdd/nas/restic'
+runx 'b2:bengalfox-backups:/'
+REST_SERVER_PASSWORD="$(cat /mnt/keydisk/rest-server-password | tr -d '\r\n\t ')"
+runx "rest:https://nas:$REST_SERVER_PASSWORD@icefox.doridian.net:nas/main"
