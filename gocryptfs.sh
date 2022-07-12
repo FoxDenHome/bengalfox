4865fb0d61e9e03a66572eaea3d518809234dbd0
Doridian <git@doridian.net>
2022-07-12 08:30:59 -0700

    Exclude things

--- gocryptfs.sh
+++ gocryptfs.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-umount /mnt/gocryptfs/nas
+umount /mnt/gocryptfs/nas || true
 gocryptfs --reverse --passfile /mnt/keydisk/gocryptfs-icefox --exclude torrent --exclude usenet /mnt/zhdd/nas /mnt/gocryptfs/nas
 
 rsync -avogXAE --progress /mnt/gocryptfs/nas/ nas@icefox.doridian.net:/mnt/zhdd/nas/share/
