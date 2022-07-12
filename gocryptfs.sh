4a5b4d21a92a15e3db1ab7af1765504547888384
Doridian <git@doridian.net>
2022-07-12 08:39:40 -0700

    rsync opt

--- gocryptfs.sh
+++ gocryptfs.sh
@@ -5,6 +5,6 @@ set -e
 umount /mnt/gocryptfs/nas || true
 gocryptfs --reverse --passfile /mnt/keydisk/gocryptfs-icefox --exclude torrent --exclude usenet /mnt/zhdd/nas /mnt/gocryptfs/nas
 
-rsync --delete -avogXAE --progress /mnt/gocryptfs/nas/ nas@icefox.doridian.net:/mnt/zhdd/nas/share/
+rsync --delete -avogXAE --numeric-ids -e 'ssh -T -o Compression=no -x' --progress /mnt/gocryptfs/nas/ nas@icefox.doridian.net:/mnt/zhdd/nas/share/
 
 umount /mnt/gocryptfs/nas
