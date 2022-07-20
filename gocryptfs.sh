1271f45a22d58b37591e712daec551603296ac2f
Doridian <git@doridian.net>
2022-07-19 22:52:12 -0700

    enhance

--- gocryptfs.sh
+++ gocryptfs.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-set -e
+set -ex
 
-umount /mnt/gocryptfs/nas || true
-gocryptfs --reverse --passfile /mnt/keydisk/gocryptfs-icefox --exclude torrent --exclude usenet /mnt/zhdd/nas /mnt/gocryptfs/nas
+umount /mnt/gocryptfs/zhdd || true
+umount /mnt/gocryptfs/zssd || true
+mkdir -p /mnt/gocryptfs/zhdd
+mkdir -p /mnt/gocryptfs/zssd
+gocryptfs --reverse --passfile /mnt/keydisk/gocryptfs-icefox --exclude nas/torrent --exclude nas/usenet /mnt/zhdd /mnt/gocryptfs/zhdd
+gocryptfs --reverse --passfile /mnt/keydisk/gocryptfs-icefox --exclude nas/torrent --exclude nas/usenet /mnt/zssd /mnt/gocryptfs/zssd
 
-rsync --delete -avogXAE --numeric-ids -e 'ssh -T -o Compression=no -x' --progress /mnt/gocryptfs/nas/ nas@icefox.doridian.net:/mnt/zhdd/nas/share/
+SYNC_COMMAND="fpsync -n 8 -f 1000  -s $((100 * 1024 * 1024)) -o '-aogXAE --numeric-ids'"
+eval $SYNC_COMMAND /mnt/gocryptfs/zssd/ nas@icefox.doridian.net:/mnt/zhdd/nas/zssd/
+eval $SYNC_COMMAND /mnt/gocryptfs/zhdd/ nas@icefox.doridian.net:/mnt/zhdd/nas/zhdd/
 
-umount /mnt/gocryptfs/nas
+umount /mnt/gocryptfs/zhdd
+umount /mnt/gocryptfs/zssd
