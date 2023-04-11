2949c2ce4163ff88cb8e3db733184f04a21c961d
Doridian <git@doridian.net>
2023-04-11 00:23:55 -0700

    modproge args

--- nvidia-reload.sh
+++ nvidia-reload.sh
@@ -9,7 +9,10 @@ rmmod nvidia_uvm nvidia_drm nvidia_modeset nvidia
 rmmod nvidia_uvm nvidia_drm nvidia_modeset nvidia
 rmmod nvidia_uvm nvidia_drm nvidia_modeset nvidia
 
-modprobe nvidia_uvm nvidia_drm nvidia_modeset nvidia
+modprobe nvidia_uvm
+modprobe nvidia_drm
+modprobe nvidia_modeset
+modprobe nvidia
 
 docker start steam_steam-headless_1
 docker start jupyter_notebook_1
