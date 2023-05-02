bec171bf87ed286a93b41fe2cb82e792ebd0943a
Doridian <git@doridian.net>
2023-05-02 11:43:46 -0700

    ups

--- restic.sh
+++ restic.sh
@@ -9,4 +9,4 @@ runx() {
 
 runx 'b2:bengalfox-backups:/' "$@"
 REST_SERVER_PASSWORD="$(cat /mnt/keydisk/rest-server-password | tr -d '\r\n\t ')"
-runx "rest:https://nas:$REST_SERVER_PASSWORD@icefox.doridian.net:8000/nas/main" "$@"
+runx "rest:https://bengalfox:$REST_SERVER_PASSWORD@icefox.doridian.net:8000/bengalfox/main" "$@"
