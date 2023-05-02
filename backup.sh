93c1df3446fcf032882ce6867f7ff9364a8391a7
Doridian <git@doridian.net>
2023-05-02 11:38:33 -0700

    ups

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/opt/bengalfox/restic.sh /opt/backup/run.sh /mnt/zhdd/docker /var/lib/docker/volumes /boot/efi /mnt/zhdd/nashome /
+/opt/bengalfox/restic.sh /opt/backup/run.sh /mnt/zhdd/docker /var/lib/docker/*/volumes /boot/efi /mnt/zhdd/nashome /
