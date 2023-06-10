0a26d8283f96c73e280d5595c966d293de8295f3
Doridian <git@doridian.net>
2023-06-10 13:21:14 -0700

    s3be

--- restic.sh
+++ restic.sh
@@ -7,6 +7,6 @@ runx() {
     "$CMD" "$@"
 }
 
-runx 'b2:bengalfox-backups:/' "$@"
+runx 's3:s3.us-west-001.backblazeb2.com/bengalfox-backups' "$@"
 REST_SERVER_PASSWORD="$(cat /mnt/keydisk/rest-server-password | tr -d '\r\n\t ')"
 runx "rest:https://bengalfox:$REST_SERVER_PASSWORD@icefox.doridian.net:8000/bengalfox/main" "$@"
