50308ddb100bfdde1e85e07496928111dc388416
Doridian <git@doridian.net>
2023-09-15 15:33:52 -0700

    fixperms

--- fix-perms.sh
+++ fix-perms.sh
@@ -2,9 +2,10 @@
 set -e
 
 fixperms() {
+	find ! -user share -print -exec chown share {} \;
+	find ! -group share -print -exec chgrp share {} \;
 	find ! -perm 0664 -type f -print -exec chmod 664 {} \;
 	find ! -perm 2775 -type d -print -exec chmod 2775 {} \;
-	find ! -group share -print -exec chgrp share {} \;
 }
 
 cd /mnt/zhdd/nas
