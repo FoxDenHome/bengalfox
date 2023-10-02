5a58cdf8801b2406c4262c4746c354eb3556cead
Doridian <git@doridian.net>
2023-10-02 15:49:22 -0700

    Add tapemgr stuff

---
+++ backup-tapes.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+exec /opt/bengalfox/tapemgr.sh store /mnt/zhdd/nas/apps /mnt/zhdd/nas/games /mnt/zhdd/nas/misc /mnt/zhdd/nas/movies /mnt/zhdd/nas/music /mnt/zhdd/nas/shows /mnt/zhdd/nashome/doridian
