d73356a3bc3221ee872c33fb94c2c03854c93240
Doridian <git@doridian.net>
2023-10-02 23:22:00 -0700

    add drive2 script

---
+++ tapemgr2.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+set -e
+cd /opt/tapemgr
+exec  python3 . '--device=/dev/tape/by-path/pci-0000:06:00.0-sas-phy7-lun-0-nst' '--changer=/dev/tape/by-path/pci-0000:06:00.0-sas-phy3-lun-1-changer' '--changer-drive-index=1' '--age-recipients=/mnt/keydisk/tape-age.pub' "$@"
