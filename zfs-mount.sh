36f23e69f4080376bb9b67ad6c2854c21b3d987b
Doridian <git@doridian.net>
2024-03-13 20:08:00 -0700

    always import, bind mount after

--- zfs-mount.sh
+++ zfs-mount.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-#zpool import -d /dev/disk/by-id -a
+zpool import -d /dev/disk/by-id -a
+
 zfs mount -a -l && exit 0
 sleep 5
 zfs mount -a -l && exit 0
 sleep 5
 zfs mount -a -l && exit 0
+
+mount --bind /mnt/zssd/nas/torrent /mnt/zhdd/nas/torrent
+mount --bind /mnt/zssd/nas/usenet /mnt/zhdd/nas/usenet
+mount --bind /mnt/zssd/nas/tmp /mnt/zhdd/nas/tmp
