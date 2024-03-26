edaf003f3d60267793044b1c1e279f6619e9a58f
Doridian <git@doridian.net>
2024-03-26 12:07:40 -0700

    new CTs

--- nvidia-reload.sh
+++ nvidia-reload.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-docker stop steam_steam-headless_1
-docker stop hashtopolis-agent_agent_1
-docker stop jupyter_notebook_1
+docker stop hashtopolis-agent_gpu_1
 docker stop nas_jellyfin_1
 
 rmmod nvidia_uvm nvidia_drm nvidia_modeset nvidia
@@ -15,7 +13,5 @@ modprobe nvidia_drm
 modprobe nvidia_modeset
 modprobe nvidia
 
-docker start steam_steam-headless_1
-docker start hashtopolis-agent_agent_1
-docker start jupyter_notebook_1
+docker start hashtopolis-agent_gpu_1
 docker start nas_jellyfin_1
