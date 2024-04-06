ac5547849518ae3226b0e724852224638345d5ce
Doridian <git@doridian.net>
2024-04-06 13:13:26 -0700

    Begin adding syncoid sync

--- backup.sh
+++ backup.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
 
 /opt/bengalfox/restic.sh /opt/backup/run.sh /mnt/zhdd/docker /var/lib/docker/*/volumes /boot/efi /mnt/zhdd/nashome /
-/opt/bengalfox/gocryptfs.sh
+
+exit 0
+
+backup_syncoid() {
+    syncoid --no-privilege-elevation "$1/ROOT/$2" "bengalfox@icefox.doridian.net:ztank/ROOT/$1/$2"
+}
+
+syncoid_zhdd() {
+    backup_syncoid zhdd "$1"
+}
+
+syncoid_zssd() {
+    backup_syncoid zssd "$1"
+}
+
+syncoid_zhdd dav
+syncoid_zhdd docker
+syncoid_zhdd e621
+syncoid_zhdd kiwix
+syncoid_zhdd nas
+syncoid_zhdd nashome
+
+syncoid_zssd docker
