cf123b34eacbd97df990232adf020d8651d94816
Doridian <git@doridian.net>
2024-04-07 09:38:41 -0700

    rm bind mounts

--- zfs-mount.sh
+++ zfs-mount.sh
@@ -2,9 +2,6 @@
 zpool import -d /dev/disk/by-id -a
 
 zdone() {
-    mount --bind /mnt/zssd/nas/torrent /mnt/zhdd/nas/torrent
-    mount --bind /mnt/zssd/nas/usenet /mnt/zhdd/nas/usenet
-    mount --bind /mnt/zssd/nas/tmp /mnt/zhdd/nas/tmp
     exit 0
 }
 
