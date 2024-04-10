c50eb7d13478117a65f695787ad494e25e11a085
Doridian <git@doridian.net>
2024-04-09 21:44:22 -0700

    fix stuff

--- backup.sh
+++ backup.sh
@@ -2,12 +2,6 @@
 
 /opt/bengalfox/restic.sh /opt/backup/run.sh /mnt/zhdd/docker /var/lib/docker/*/volumes /boot/efi /mnt/zhdd/nashome /
 
-exit 0
-
-backup_syncoid_root() {
-    syncoid --no-privilege-elevation "$1/ROOT" "bengalfox@icefox.doridian.net:ztank/ROOT/$1"
-}
-
 backup_syncoid() {
     syncoid --no-privilege-elevation "$1/ROOT/$2" "bengalfox@icefox.doridian.net:ztank/ROOT/$1/$2"
 }
@@ -20,13 +14,11 @@ syncoid_zssd() {
     backup_syncoid zssd "$1"
 }
 
-backup_syncoid_root zhdd
 syncoid_zhdd dav
 syncoid_zhdd docker
-syncoid_zhdd e621
+#syncoid_zhdd e621
 syncoid_zhdd kiwix
 syncoid_zhdd nas
 syncoid_zhdd nashome
 
-backup_syncoid_root zssd
 syncoid_zssd docker
