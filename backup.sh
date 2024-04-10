89f16434e42adf4d4818a2bec247d8d98694f9e2
Doridian <git@doridian.net>
2024-04-09 21:43:27 -0700

    backup roots

--- backup.sh
+++ backup.sh
@@ -4,6 +4,10 @@
 
 exit 0
 
+backup_syncoid_root() {
+    syncoid --no-privilege-elevation "$1/ROOT" "bengalfox@icefox.doridian.net:ztank/ROOT/$1"
+}
+
 backup_syncoid() {
     syncoid --no-privilege-elevation "$1/ROOT/$2" "bengalfox@icefox.doridian.net:ztank/ROOT/$1/$2"
 }
@@ -16,6 +20,7 @@ syncoid_zssd() {
     backup_syncoid zssd "$1"
 }
 
+backup_syncoid_root zhdd
 syncoid_zhdd dav
 syncoid_zhdd docker
 syncoid_zhdd e621
@@ -23,4 +28,5 @@ syncoid_zhdd kiwix
 syncoid_zhdd nas
 syncoid_zhdd nashome
 
+backup_syncoid_root zssd
 syncoid_zssd docker
