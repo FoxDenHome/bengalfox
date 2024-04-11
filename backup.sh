b07667c5f2b65bf4a3d4791b176f1bba1af29472
Doridian <git@doridian.net>
2024-04-11 10:22:51 -0700

    never resume

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 /opt/bengalfox/restic.sh /opt/backup/run.sh /mnt/zhdd/docker /var/lib/docker/*/volumes /boot/efi /mnt/zhdd/nashome /
 
 backup_syncoid() {
-    syncoid --no-privilege-elevation "$1/ROOT/$2" "bengalfox@icefox.doridian.net:ztank/ROOT/$1/$2"
+    syncoid --no-resume --no-privilege-elevation "$1/ROOT/$2" "bengalfox@icefox.doridian.net:ztank/ROOT/$1/$2"
 }
 
 syncoid_zhdd() {
@@ -16,7 +16,7 @@ syncoid_zssd() {
 
 syncoid_zhdd dav
 syncoid_zhdd docker
-#syncoid_zhdd e621
+syncoid_zhdd e621
 syncoid_zhdd kiwix
 syncoid_zhdd nas
 syncoid_zhdd nashome
