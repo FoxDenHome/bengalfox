7f39b28199ca74a6f0a8ba6863e74567447f24d7
Doridian <git@doridian.net>
2024-05-03 20:10:49 -0700

    Not bash

--- sriov-manual.sh
+++ sriov-manual.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-set -euo pipefail
-set -x
+#!/bin/sh
+set -ex
 
-ip link set dev 'enp129s0f0np0' vf '126' mac '5e:8c:f2:cd:c8:4a' vlan '2' spoofchk on
+ip link set dev 'enp129s0f0np0' vf '126' mac '5e:8c:f2:cd:c8:4a' vlan '0' spoofchk on
