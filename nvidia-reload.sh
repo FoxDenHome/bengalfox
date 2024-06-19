22c8fc7f9962ff0371e8812baa620e19b9f568d6
Doridian <git@doridian.net>
2024-06-18 21:24:51 -0700

    new naming

--- nvidia-reload.sh
+++ nvidia-reload.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-docker stop hashtopolis-agent_gpu_1
-docker stop nas_jellyfin_1
+docker stop hashtopolis-agent-gpu-1
+docker stop nas-jellyfin-1
 
 rmmod nvidia_uvm nvidia_drm nvidia_modeset nvidia
 rmmod nvidia_uvm nvidia_drm nvidia_modeset nvidia
@@ -13,5 +13,5 @@ modprobe nvidia_drm
 modprobe nvidia_modeset
 modprobe nvidia
 
-docker start hashtopolis-agent_gpu_1
-docker start nas_jellyfin_1
+docker start hashtopolis-agent-gpu-1
+docker start nas-jellyfin-1
