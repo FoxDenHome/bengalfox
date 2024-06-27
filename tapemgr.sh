670d7ea68dd89ac81e7ca7b9838b78ab8d39d2f4
Doridian <git@doridian.net>
2024-06-27 15:39:04 -0700

    a

--- tapemgr.sh
+++ tapemgr.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 cd /opt/tapemgr
-exec  python3 . '--changer=/dev/tape/by-id/scsi-1BDT_FlexStor_II_00L2U78BK968_LL0' '--changer-drive-index=0' '--age-recipients=/mnt/keydisk/tape-age.pub' "$@"
+exec  python3 . '--changer=/dev/sch0' '--changer-drive-index=0' '--age-recipients=/mnt/keydisk/tape-age.pub' "$@"
