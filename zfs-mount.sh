626fe544c4c9de10ec7842a3ad690eb817d680a9
Doridian <git@doridian.net>
2024-07-05 22:32:27 -0700

    Revert "rm bind mounts"
    This reverts commit cf123b34eacbd97df990232adf020d8651d94816.

--- zfs-mount.sh
+++ zfs-mount.sh
@@ -2,6 +2,9 @@
 zpool import -d /dev/disk/by-id -a
 
 zdone() {
+    mount --bind /mnt/zssd/nas/torrent /mnt/zhdd/nas/torrent
+    mount --bind /mnt/zssd/nas/usenet /mnt/zhdd/nas/usenet
+    mount --bind /mnt/zssd/nas/tmp /mnt/zhdd/nas/tmp
     exit 0
 }
 
