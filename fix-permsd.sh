57ec71aff0079b47dd41d3c1d4d83032d5138009
Doridian <git@doridian.net>
2024-11-15 22:20:01 -0800

    Also chmod

--- fix-permsd.sh
+++ fix-permsd.sh
@@ -4,8 +4,8 @@ inotifywait -P -e attrib,create --format '%w%f%0' --no-newline -r /mnt/zhdd/nas 
     do
         chown -h share:share "$file"
         if [ -d "$file" ]; then
-            chmod 2775 "$file"
+            chmod -h 2775 "$file"
         else
-            chmod 664 "$file"
+            chmod -h 664 "$file"
         fi
     done
