32ecb302ecef9940af58f8ca7fb8ab2f4ef81302
Doridian <git@doridian.net>
2024-11-15 22:15:35 -0800

    chg

--- fix-permsd.sh
+++ fix-permsd.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-inotifywait -P -e attrib,create --format '%w%f%0' --no-newline -r /mnt/zhdd/nas -m | xargs -0 chown -h share:share
+inotifywait -P -e attrib,create --format '%w%f%0' --no-newline -r /mnt/zhdd/nas -m | \
+    while IFS= read -r -d '' file
+    do
+        chown -h share:share "$file"
+    done
